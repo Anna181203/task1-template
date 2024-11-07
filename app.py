a8592771a9d6312d0ea3111b68c7117c543ef3ad
Anna181203 <alippold@uni-osnabrueck.de>
2024-11-07 14:55:40 +0100

    Testing out how it works

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Hello world")
 
-st.write("This is my first web app. And it is not working")
+st.write("This is my first web app. And it is not working, why")
 st.button ("click me")
