ce88608e8331d2175dd77e0b8fcfedd84aaca65f
Anna181203 <alippold@uni-osnabrueck.de>
2024-11-07 16:03:47 +0100

    page title and icon

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import random
 
+#Page name and icon
 st.set_page_config(page_title="Animal Guessing Game", page_icon=":paw_prints:")
+
+#Introduction to the game
+st.write("Welcome to the Animal Guessing Game!")
+st.write("Please guess the animal I am thinking about. The options are:")
+st.write("fly, spider, mouse, rat, bird, rabbit, monkey, cat, racoon,")
+st.write ("fox, pig, panda, dog, wolf, lion, horse, giraffe, elephant, whale")
