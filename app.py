40e8230a1b1a0524b7d4d883056475432564746d
Anna181203 <alippold@uni-osnabrueck.de>
2024-11-10 15:30:41 +0100

    Finished all Animal Categories

--- app.py
+++ app.py
@@ -8,26 +8,26 @@ st.set_page_config(page_title="Animal Guessing Game", page_icon=":paw_prints:")
 st.write("Welcome to the Animal Guessing Game!")
 st.write("Please guess the animal I am thinking about. The options are:")
 st.write("fly, spider, mouse, rat, bird, rabbit, monkey, cat, racoon,")
-st.write ("fox, pig, panda, dog, wolf, lion, horse, giraffe, elephant, whale, test")
+st.write ("fox, pig, panda, dog, wolf, lion, horse, giraffe, elephant, whale")
 
 animals = {
-    "Fly": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Spider": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Mouse": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Rat": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Bird": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Rabbit": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Monkey": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Cat": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Racoon": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Fox": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Pig": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Panda": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Dog": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Wolf": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Lion": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Horse": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Giraffe": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Elephant": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""},
-    "Whale": {"Habitat":"", "Size":"", "Food":"", "Movement":"", "Color":"", "Reproduction:":""}
+    "Fly": {"Habitat":"Everywhere", "Size":"XS", "Food":"Omnivore", "Movement":"Flying", "Color":"Black", "Reproduction:":"Oviparous"},
+    "Spider": {"Habitat":"Everywhere", "Size":"XS", "Food":"Carnivore", "Movement":"Walking", "Color":"Black", "Reproduction:":"Oviparous"},
+    "Mouse": {"Habitat":"Everywhere", "Size":"XS", "Food":"Omnivore", "Movement":"Walking", "Color":"Grey", "Reproduction:":"Mammal"},
+    "Rat": {"Habitat":"Everywhere", "Size":"S", "Food":"Omnivore", "Movement":"Walking", "Color":"Grey", "Reproduction:":"Mammal"},
+    "Bird": {"Habitat":"Everywhere", "Size":"S", "Food":"Omnivore", "Movement":"Flying", "Color":"Colorful", "Reproduction:":"Oviparous"},
+    "Rabbit": {"Habitat":"Everywhere", "Size":"S", "Food":"Herbivore", "Movement":"Hopping", "Color":"Brown", "Reproduction:":"Mammal"},
+    "Monkey": {"Habitat":"Africa", "Size":"M", "Food":"Omnivore", "Movement":"Climbing", "Color":"Brown", "Reproduction:":"Mammal"},
+    "Cat": {"Habitat":"Everywhere", "Size":"M", "Food":"Carnivore", "Movement":"Walking", "Color":"Multi", "Reproduction:":"Mammal"},
+    "Racoon": {"Habitat":"America", "Size":"M", "Food":"Omnivore", "Movement":"Walking", "Color":"Grey", "Reproduction:":"Mammal"},
+    "Fox": {"Habitat":"Everywhere", "Size":"M", "Food":"Omnivore", "Movement":"Walking", "Color":"Red", "Reproduction:":"Mammal"},
+    "Pig": {"Habitat":"Everywhere", "Size":"L", "Food":"Omnivore", "Movement":"Walking", "Color":"Brown", "Reproduction:":"Mammal"},
+    "Panda": {"Habitat":"Asia", "Size":"L", "Food":"Herbivore", "Movement":"Climbing", "Color":"White", "Reproduction:":"Mammal"},
+    "Dog": {"Habitat":"Everywhere", "Size":"L", "Food":"Carnivore", "Movement":"Walking", "Color":"Multi", "Reproduction:":"Mammal"},
+    "Wolf": {"Habitat":"Europe", "Size":"L", "Food":"Carnivore", "Movement":"Walking", "Color":"Grey", "Reproduction:":"Mammal"},
+    "Lion": {"Habitat":"Africa", "Size":"L", "Food":"Carnivore", "Movement":"Walking", "Color":"Brown", "Reproduction:":"Mammal"},
+    "Horse": {"Habitat":"America", "Size":"L", "Food":"Herbivore", "Movement":"Walking", "Color":"Multi", "Reproduction:":"Mammal"},
+    "Giraffe": {"Habitat":"Africa", "Size":"XL", "Food":"Herbivore", "Movement":"Walking", "Color":"Brown", "Reproduction:":"Mammal"},
+    "Elephant": {"Habitat":"Africa", "Size":"XL", "Food":"Herbivore", "Movement":"Walking", "Color":"Grey", "Reproduction:":"Mammal"},
+    "Whale": {"Habitat":"Ocean", "Size":"XL", "Food":"Carnivore", "Movement":"Swimming", "Color":"Grey", "Reproduction:":"Mammal"}
 }
